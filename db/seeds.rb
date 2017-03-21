de145ec103977fbe23fab88b62fd6bd9117b47fa
Cesurhan <ces-ur@hotmail.com>
2017-03-21 14:25:53 +0100

    Generate product model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+user1 = User.create!(email: "fake@bigdreams.com", password: "qwe123")
+
+puts "#{User.all.size} users created"
+
+# Dreams
+dream1 = Product.create!(
+name: "Millionaire's Dream",
+description: "You want to have all? Be millionaire for a night",
+price: 100,
+photo: "http://blogs-images.forbes.com/timmaurer/files/2015/06/rich.jpg")
+
+dream2 = Product.create!(
+name: "Senior Web Developer",
+description: "Master all the programming languages in one night!",
+price: 150,
+photo: "http://techiegrp.com/wp-content/uploads/2014/05/Senior-Web-Developer.jpg")
+
+puts "#{Product.all.size} dreams created"
