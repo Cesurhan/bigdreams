08365f5339a43a5f491cbd82abc50bc50aef6515
marcia1983 <marcia.albrecht.lorenzini@gmail.com>
2017-03-24 07:42:47 +0100

    Add user and product validations and test for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   mount_uploader :photo, ImageUploader
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
 end
